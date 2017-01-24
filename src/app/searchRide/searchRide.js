2ed08a3dba3d72a2137c1ef159f6024b5115563e
exp-christy <christy.mathew@experionglobal.com>
2017-01-24 19:05:07 +0530

    template url changed

--- src/app/searchRide/searchRide.js
+++ src/app/searchRide/searchRide.js
@@ -1,7 +1,7 @@
 angular
   .module('app')
   .component('searchRide', {
-    templateUrl: 'searchRide/searchRide.html',
+    templateUrl: '/app/searchRide/searchRide.html',
     controller: 'SearchRideController',
 	controllerAs:'ctrl',
     bindings: {
