8b42e521729f5f4cc4b1483c94aa97a514522191
exp-ashique <muhammed.ashique@experionglobal.com>
2017-01-24 17:04:37 +0530

    create ride js

---
+++ src/app/createride/createride.js
@@ -0,0 +1,11 @@
+angular
+  .module('app')
+  .component('createRide', {
+    templateUrl: '/app/createride/createride.html',
+    controller: 'crController',
+    controllerAs: 'ctrl',
+    bindings: {
+    
+
+    }
+  });
