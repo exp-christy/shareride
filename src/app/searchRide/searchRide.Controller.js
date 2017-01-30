084bd75c65c936cffddfb163c316530a39369a0c
exp-christy <christy.mathew@experionglobal.com>
2017-01-30 13:41:38 +0530

    comparision changed to avoid casesensitive problem

--- src/app/searchRide/searchRide.Controller.js
+++ src/app/searchRide/searchRide.Controller.js
@@ -7,19 +7,21 @@ function SearchRideController($http) {
   ctrl.searchResult = [];
   ctrl.searchRideDetails = searchRideDetails;
 
+  //Function searches ride details for the place from JSON file
   function searchRideDetails(place) {
-    $http.get("/data/rideDetails.json")
+    if( typeof place === 'undefined'){
+      $http.get("/data/rideDetails.json")
       .then(function (response) {
         var res = response.data.rides;
         var len = res.length;
         ctrl.searchResult = [];
         for(var i=0;i < len; i++){
-            if(res[i].from === place){
+            if(res[i].from.toUpperCase() == place.toUpperCase()){
                 ctrl.searchResult.push(angular.copy(res[i]));
             }
         }
         res=[];
       });
-
+    }
   };
 }
