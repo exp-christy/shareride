535c0d55e7d20c3e5b88ddcc4caa8b49d52d7d86
Dona <Dona.thomas@experionglobal.com>
2017-02-01 18:42:52 +0530

    sdf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 angular
   .module('app', ['ui.router','ui.bootstrap', 'ngAnimate', 'toastr', 'firebase']);
-
-  
-
 var config = {
   apiKey: "AIzaSyCexbbGcO-lskcQ2bhMK2qVQRpRKX2_aCA",
   authDomain: "sample-95dc3.firebaseapp.com",
