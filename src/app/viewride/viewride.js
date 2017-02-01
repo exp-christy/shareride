4dba06f198d7a32c093a2c49f2102739ab2f4e80
exp-christy <christy.mathew@experionglobal.com>
2017-02-01 09:43:14 +0530

    Moved folders

--- src/app/viewride/viewride.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('app')
-  .component('viewRide', {
-    templateUrl: 'app/viewride/viewride.html',
-    controller: 'ViewRideController',
-    controllerAs: 'ctrl',
-    bindings: {}  
-  });
