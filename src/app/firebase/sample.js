dc5e4f671b85633cb8589a5aa7b040a1c4dad6c8
Basilin Joe <basilin.joe@experionglobal.com>
2017-02-01 15:51:13 +0530

    Add update on doubleclick @sample firebase app

--- src/app/firebase/sample.js
+++ src/app/firebase/sample.js
@@ -9,13 +9,30 @@
 
     function fireBaseController($firebaseArray) {
       var ctrl = this;
-      var ref = firebase.database().ref().child("datas");
-      console.log(ref);
-      ctrl.onSubmit = onSubmit;
-      ctrl.datas = $firebaseArray(ref);
+      var ref = firebase.database().ref().child("datas"); // get reference
+
+      ctrl.onSubmit = onSubmit; // function to add a record
+      ctrl.remove = remove; // function to remove a record
+      ctrl.selectItem = selectItem; // function to remove a record
+      ctrl.datas = $firebaseArray(ref); // data array
+      ctrl.formData = {};
 
       function onSubmit(data) {
-        ctrl.datas.$add(data);
+        if (data.$id) {
+          ctrl.datas.$save(data);
+        } else {
+          ctrl.datas.$add(data);
+        }
+        ctrl.formData = {};
+      }
+
+      function remove(data) {
+        ctrl.datas.$remove(data);
+      }
+
+      function selectItem(data) {
+        console.log('Working ...');
+        ctrl.formData = data;
       }
     }
 
