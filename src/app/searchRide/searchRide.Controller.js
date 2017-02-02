680fea9fed3ee7409fba416f7d9cc551f68640ac
Dona <Dona.thomas@experionglobal.com>
2017-02-02 19:01:59 +0530

    Typeahead feature for search box added

--- src/app/searchRide/searchRide.Controller.js
+++ src/app/searchRide/searchRide.Controller.js
@@ -2,28 +2,17 @@ angular
   .module('app')
   .controller('SearchRideController', SearchRideController);
 
-function SearchRideController($http,$state) {
+function SearchRideController($http, $state) {
   var ctrl = this;
-   ctrl.startLocations = [];
-    ctrl.$onInit = init;
+  ctrl.startLocations = [];
   ctrl.searchRideDetails = searchRideDetails;
-  function searchRideDetails(place){
-    $state.go('rideList',{'fromPlace':place});
-  }
-  function init() {
-      $http.get('/data/rideDetails.json').then(function (response) {
-      var rideDetails = response.data.rides;
-      var len = rideDetails.length;
-      for (var i = 0; i < len; i++) {
-        ctrl.startLocation = rideDetails[i].from;
-        if(i>0)
-        var repeatLoc  = ctrl.startLocation.localeCompare(startLocations[i]);
-        if(repeatLoc!==0){
-        ctrl.startLocations.push(angular.copy(ctrl.startLocation));   
-        }  
-      }
-      console.log(ctrl.startLocations);
+  var same = 0;
+
+  function searchRideDetails(place) {
+    $state.go('rideList', {
+      'fromPlace': place
     });
   }
-    
+ ctrl.places = ["pala","Piravom","Kochi","Kottayam","Perumbaavoor"];
+
 }
