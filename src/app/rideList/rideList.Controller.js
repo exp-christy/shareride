6ecadc04d72c1710b56dacc1102124873bd26b41
exp-christy <christy.mathew@experionglobal.com>
2017-02-06 14:59:10 +0530

    Backend implemented for create ride

--- src/app/rideList/rideList.Controller.js
+++ src/app/rideList/rideList.Controller.js
@@ -1,31 +1,31 @@
 angular
     .module('app')
-    .controller('RideListController',RideListController);
+    .controller('RideListController', RideListController);
 
-function RideListController($http,$stateParams,$state,$timeout,$firebaseArray){
-    var ctrl = this;
+function RideListController($http, $stateParams, $state, $timeout, $firebaseArray) {
+  var ctrl = this;
   ctrl.searchResult = [];
   ctrl.viewRideDetails = viewRideDetails;
-  ctrl.filterDetail= {};
+  ctrl.filterDetail = {};
   ctrl.$onInit = init;
   var ref = firebase.database().ref().child("rideDetailsList");
-  ctrl.rides = $firebaseArray(ref);
-  ctrl.ampm;
-  
-  //Function searches ride details for the place from JSON file
-  function init(){
-    
+  ctrl.rideDetailsList = $firebaseArray(ref);
+  //  Function searches ride details for the place from JSON file
+  function init() {
     ctrl.fromPlace = $stateParams.fromPlace.toUpperCase();
-    ctrl.searchResult = [];
-        for(var i=0;i < ctrl.len; i++){
-              if(ctrl.rides[i].from.toUpperCase() == $stateParams.fromPlace.toUpperCase()){
-                ctrl.searchResult.push(angular.copy(ctrl.rides[i]));
+    ctrl.rideDetailsList.$loaded().then(function () {
+      ctrl.len = ctrl.rideDetailsList.length;
+      ctrl.searchResult = [];
+      for (var i = 0 ; i < ctrl.len ; i++) {
+              if(ctrl.rideDetailsList[i].from.toUpperCase() == $stateParams.fromPlace.toUpperCase()){
+                ctrl.searchResult.push(angular.copy(ctrl.rideDetailsList[i]));
               }
-        }
+      }
+    });     
   }
   
-  //Function to go to viewride page
+  //  Function to go to viewride page
   function viewRideDetails(Id){
-    $state.go('viewride',{'rideID':Id});
+    $state.go('viewride',{'ride':Id});
   }
 }
