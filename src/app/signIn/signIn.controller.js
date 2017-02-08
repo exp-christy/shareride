dbcc82fd291b49276fbaf221f859013a246ac18b
Dona <Dona.thomas@experionglobal.com>
2017-02-08 11:34:03 +0530

    login and registration not completed yet

--- src/app/signIn/signIn.controller.js
+++ src/app/signIn/signIn.controller.js
@@ -1,6 +1,17 @@
 angular.module("app")
   .controller("SignInController", SignInController);
 
-function SignInController() {
-
+function SignInController($uibModal) {
+  var ctrl = this;
+  ctrl.openComponentModal = openComponentModal;
+  function openComponentModal() {
+    var modalInstance = $uibModal.open({
+      component: 'userLogin'
+    });
+    modalInstance.result.then(function (formData) {
+      $log.info(formData);
+    }).catch(function () {
+      $log.info('modal-component dismissed at: ' + new Date());
+    });
+  }
 }
