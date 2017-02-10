69f2cf530fda475c8d1a7f35243df3f7256389f1
exp-christy <christy.mathew@experionglobal.com>
2017-02-10 17:25:55 +0530

    header selection for ridelist

--- src/app/myHeader/myHeader.controller.js
+++ src/app/myHeader/myHeader.controller.js
@@ -2,6 +2,7 @@ angular.module("app")
   .controller("HeaderController", HeaderController);
 
 function HeaderController($firebaseAuth) {
+  var ctrl = this;
   ctrl.authObj = $firebaseAuth();
   ctrl.firebaseUser = ctrl.authObj.$getAuth();
 }
--- src/app/rideList/rideList.Controller.js
+++ src/app/rideList/rideList.Controller.js
@@ -14,16 +14,27 @@ function RideListController($http, $stateParams, $state,toastr, $timeout, $fireb
   //  Function searches ride details for the place from JSON file
   function init() {
     ctrl.firebaseUser = ctrl.authObj.$getAuth();
+    if(ctrl.firebaseUser){
+      ctrl.showUserHeader = true;
+      ctrl.showMyHeader = false;
+    }
+    else{
+      ctrl.showUserHeader = false;
+      ctrl.showMyHeader = true;
+
+    }
     ctrl.fromPlace = $stateParams.fromPlace.toUpperCase();
-    ctrl.rideDetailsList.$loaded().then(function () {
-      ctrl.len = ctrl.rideDetailsList.length;
-      ctrl.searchResult = [];
-      for (var i = 0; i < ctrl.len; i++) {
-        if (ctrl.rideDetailsList[i].from.toUpperCase() === $stateParams.fromPlace.toUpperCase()) {
-          ctrl.searchResult.push(angular.copy(ctrl.rideDetailsList[i]));
+    if(angular.isDefined($stateParams.fromPlace)){
+      ctrl.rideDetailsList.$loaded().then(function () {
+        ctrl.len = ctrl.rideDetailsList.length;
+        ctrl.searchResult = [];
+        for (var i = 0; i < ctrl.len; i++) {
+          if (ctrl.rideDetailsList[i].from.toUpperCase() === $stateParams.fromPlace.toUpperCase()) {
+            ctrl.searchResult.push(angular.copy(ctrl.rideDetailsList[i]));
+          }
         }
-      }
-    });
+      });
+    }
   }
   //  Function to go to viewride page
   function viewRideDetails(Id) {
