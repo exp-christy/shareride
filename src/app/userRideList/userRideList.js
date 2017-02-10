63f72fbbdd1c46ce2ccccc73eb2fca56f8b48f5f
exp-christy <christy.mathew@experionglobal.com>
2017-02-10 17:31:18 +0530

    duplicate list removed

--- src/app/userRideList/userRideList.js
+++
@@ -1,8 +0,0 @@
-angular
-    .module('app')
-    .component('userRideList', {
-      templateUrl: 'app/userRideList/userRideList.html',
-      controller: 'UserRideListController',
-      controllerAs: 'ctrl',
-      bindings: {}
-    });
