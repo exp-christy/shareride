fdbc29967beb5b269dae951512d9a82f8bb49417
Dona <Dona.thomas@experionglobal.com>
2017-02-10 17:33:45 +0530

    search ride improvement

--- src/app/searchRide/searchRide.Controller.js
+++ src/app/searchRide/searchRide.Controller.js
@@ -43,12 +43,15 @@ function SearchRideController($http, toastr, $state, $firebaseArray) {
         }
       }
       if (placeNotFound === 'true') {
+        console.log(placeNotFound,angular.isDefined(place));
         ctrl.formData.place = "";
         toastr.info('Sorry no rides were found!');
         ctrl.frmSearchRide.$setUntouched();
+        ctrl.frmSearchRide.$setPristine();
       }
     }
     else {
+      console.log(placeNotFound,angular.isDefined(place));
       toastr.info('Please type some place and search!');
     }
   }
