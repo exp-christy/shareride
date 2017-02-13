ae46ce4b9c61e9bae0e639f51ea415affe4dfe88
Dona <Dona.thomas@experionglobal.com>
2017-02-13 16:07:44 +0530

    viewModa0

--- src/app/viewride/viewModal/viewmodal.Controller.js
+++ src/app/viewride/viewModal/viewmodal.Controller.js
@@ -1,11 +1,16 @@
 angular.module("app")
   .controller("ViewModalController", ViewModalController);
 
-function ViewModalController($uibModal, $state, toastr) {
+function ViewModalController($uibModal, $state, toastr,  $firebaseArray) {
   var ctrl = this;
   ctrl.formData = {};
   ctrl.rideBook = rideBook;
   ctrl.cancel = cancel;
+  ctrl.formData = {};
+  var bookRef = firebase.database().ref().child('book');
+  ctrl.bookDetail = $firebaseArray(bookRef);
+  var ref1 = firebase.database().ref().child('rideDetailsList');
+  ctrl.rideDetail = $firebaseArray(ref1);
   ctrl.ok = function () {
     ctrl.close({
       $value: ctrl.formData
@@ -22,5 +27,11 @@ function ViewModalController($uibModal, $state, toastr) {
     ctrl.cancel();
     $state.go('userHome');
     toastr.success('Booking has been made!');
+    // ctrl.formData.rideId = $stateParams.ride;
+    // ctrl.formData.firebaseUserId = ctrl.firebaseUserId;
+    // ctrl.bookDetail.$add(ctrl.formData);
+     ctrl.result = ctrl.rideDetail.$getRecord(ctrl.formData.rideId);
+     ctrl.result.vacantSeats = ctrl.result.vacantSeats - 1;
+     ctrl.rideDetail.$save(ctrl.result);
   }
 }
